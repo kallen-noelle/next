0ae767ff1a1b7754030d3527ed5aaedcf757974b
kallen-noelle <2758251905@qq.com>
2026-06-23 20:59:56 +0800

    hasdomain turn false

--- lib/siteConfig.ts
+++ lib/siteConfig.ts
@@ -34,7 +34,7 @@ export const siteConfig = {
   /** 站点域名 */
   blog: "www.lxpavilion.top",
   /** 是否有域名 */
-  hasDomain: true,
+  hasDomain: false,
 
   /** 建站日期 */
   buildDate: "2026-05-21T00:00:00",
